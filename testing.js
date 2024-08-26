1744bdacbc8943e703c51833cabda39d6fdfc4b2
muhdaidid2711 <muhd.aidid907@gmail.com>
2024-08-26 18:53:19 +0800

    added new test file

---
+++ testing.js
@@ -0,0 +1,11 @@
+const summation = (input) => {
+    // Add your code below:
+     let sum = 0;
+     let i = 1;
+
+    while (i <= input) {
+        sum += i;
+        i++
+    }
+    return sum;
+}
